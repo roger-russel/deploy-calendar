623fccec86a5682ad0617a2b99f4963f09a446e6
Roger Russel <roger@rrussel.org>
2019-05-05 17:00:33 -0300

    log output with google logger

--- pkg/session/start.go
+++ pkg/session/start.go
@@ -2,23 +2,23 @@ package session
 
 import (
 	"flag"
-	"log"
 
 	"github.com/fasthttp/session"
 	"github.com/fasthttp/session/memory"
+	"github.com/google/logger"
 )
 
 //Start Session connection session
-func Start() {
+func Start(providerName string) {
 
 	var serverSession = session.New(session.NewDefaultConfig())
 	var config session.ProviderConfig
 
 	config = &memory.Config{}
 
-	providerName := flag.String("provider", "memory", "Name of provider")
-	if errSession := serverSession.SetProvider(*providerName, config); errSession != nil {
-		log.Fatalf("Session could not start: %s", errSession)
+	providerNameFlag := flag.String("provider", "memory", providerName)
+	if errSession := serverSession.SetProvider(*providerNameFlag, config); errSession != nil {
+		logger.Fatalf("Session could not start: %s", errSession)
 	}
 
 }
