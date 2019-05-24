79c2771f6181d28549d6e7ff20d9f9f81364c4bd
Roger Russel <roger@rrussel.org>
2019-05-24 18:26:52 -0300

    merge json

---
+++ pkg/jwt/generate.go
@@ -0,0 +1,8 @@
+
+var jwtKey = []byte("my_secret_key")
+
+token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+    "foo": "bar",
+    "nbf": time.Date(2015, 10, 10, 12, 0, 0, 0, time.UTC).Unix(),
+})
+tokenString, err := token.SignedString(hmacSampleSecret)
